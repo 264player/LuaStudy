8393c2b137c949f3cf7ce6c56695829837c70d18
264player <2309707428@qq.com>
2023-09-08 19:06:11 +0800

    又水了一天，明天进入目录中的编程实操

---
+++ Code/day8.lua
@@ -0,0 +1,24 @@
+-- local i = 0 --局部变量
+-- if  then
+-- elseif  then
+-- end
+-- for i = 1, 10, 1 do
+
+-- end
+-- while true do
+
+-- end
+-- repeat
+
+-- until
+-- for i = 1, 10, 1 do--如果不想设置上限，就设置为math.huge
+
+-- end
+-- for index, value in ipairs(t) do
+
+-- end
+-- for key, value in pairs(t) do
+
+-- end
+-- ::b::
+-- goto b
